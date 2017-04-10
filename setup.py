23e73de28257a39c8976fdcb58f2e270e0d25c31
Paul Ivanov <pivanov5@bloomberg.net>
2017-04-10 15:55:31 -0700

    change authorship to IPython dev team, update URL

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='rlipython',
+    version='0.1dev',
+    packages=['rlipython',],
+    license='BSD',
+    author='The IPython Development Team',
+    author_email='ipython-dev@python.org',
+    url='https://github.com/ipython/rlipython',
+    description="readline integration for IPython 5.4+ and 6.0+",
+    long_description=open('readme.md').read(),
+)
