46516220de6560431f264cd55ef9b65d648266c7
Paul Ivanov <pivanov5@bloomberg.net>
2017-05-05 14:59:21 -0700

    install and uninstall as unique completions
    import rlipython as rl
    rl.i<tab>
    rl.u<tab>
    
    will now complete to install and uninstall, respectively.

--- rlipython/__init__.py
+++ rlipython/__init__.py
@@ -6,5 +6,5 @@ version_info = (0,0,1)
 __version__ = '.'.join([str(x) for x in version_info])
 
 from .completer import RLCompleter
-from .interactiveshell import TerminalInteractiveShell
-from .utils import install, uninstall
+from .shell import TerminalInteractiveShell
+from .lifecycle import install, uninstall
